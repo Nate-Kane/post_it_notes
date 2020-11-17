be0caa60f7ef2efd025faa2ff868567e8fef7b6a
Nathan Kane <kaneitalia.rana@gmail.com>
2020-11-16 18:23:18 -0700

    finished it

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # get 'notes/index'
+
+  # get 'notes/new'
+
+  # get 'notes/show'
+
+   root 'notes#index'
+
+   resources :notes
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
